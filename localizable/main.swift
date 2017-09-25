5e2f874884912018281bdd95b23a5d38a89ebfb0
Craig Spitzkoff <craig.spitzkoff@raizlabs.com>
2017-09-25 19:42:54 -0400

    Initial Commit

---
+++ localizable/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  localizable
+//
+//  Created by Craig Spitzkoff on 9/25/17.
+//  Copyright © 2017 Boston Synthesis. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
